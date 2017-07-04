78e0e801721b5944a1a3af0764cd040dd51ec568
Bart Romanow <bart.romanow88@gmail.com>
2017-07-04 11:55:02 +0900

    Finish Android module

--- android/src/main/java/com/theoremreach/RNTheoremReachModule.java
+++ android/src/main/java/com/theoremreach/RNTheoremReachModule.java
@@ -1,22 +1,100 @@
-
 package com.theoremreach;
 
+import android.support.annotation.Nullable;
+
+import com.facebook.react.bridge.LifecycleEventListener;
 import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.Callback;
+import com.facebook.react.modules.core.RCTNativeAppEventEmitter;
 
-public class RNTheoremReachModule extends ReactContextBaseJavaModule {
+import theoremreach.com.theoremreach.TheoremReach;
+import theoremreach.com.theoremreach.TheoremReachRewardListener;
+import theoremreach.com.theoremreach.TheoremReachSurveyListener;
 
-  private final ReactApplicationContext reactContext;
+public class RNTheoremReachModule extends ReactContextBaseJavaModule
+        implements LifecycleEventListener, TheoremReachRewardListener, TheoremReachSurveyListener {
 
-  public RNTheoremReachModule(ReactApplicationContext reactContext) {
-    super(reactContext);
-    this.reactContext = reactContext;
-  }
+    private final ReactApplicationContext reactContext;
+    private boolean isAppInitialized = false;
 
-  @Override
-  public String getName() {
-    return "RNTheoremReach";
-  }
+    public RNTheoremReachModule(ReactApplicationContext reactContext) {
+        super(reactContext);
+        this.reactContext = reactContext;
+        reactContext.addLifecycleEventListener(this);
+    }
+
+    @Override
+    public String getName() {
+        return "RNTheoremReach";
+    }
+
+    @ReactMethod
+    public void initWithApiKeyAndUserId(String apiKey, String userId) {
+        TheoremReach.initWithApiKeyAndUserIdAndActivityContext(apiKey, userId, getCurrentActivity());
+
+        // The below code is required because onResume is called before this method by default
+        // and it should be prevented for the correct working of the SDK
+        TheoremReach.getInstance().onResume(getCurrentActivity());
+        isAppInitialized = true;
+
+        TheoremReach.getInstance().setTheoremReachRewardListener(this);
+        TheoremReach.getInstance().setTheoremReachSurveyListener(this);
+    }
+
+    @ReactMethod
+    public void showRewardCenter() {
+        TheoremReach.getInstance().showRewardCenter();
+    }
+
+    @ReactMethod
+    public void isSurveyAvailable(Callback cb) {
+        cb.invoke(TheoremReach.getInstance().isSurveyAvailable());
+    }
+
+    /* Callbacks */
+
+    private void sendEvent(ReactContext reactContext,
+                           String eventName,
+                           @Nullable Object params) {
+        reactContext
+                .getJSModule(RCTNativeAppEventEmitter.class)
+                .emit(eventName, params);
+    }
+
+    @Override
+    public void onReward(int quantity) {
+        sendEvent(this.reactContext, "onReward", quantity);
+    }
+
+    @Override
+    public void onRewardCenterOpened() {
+        sendEvent(this.reactContext, "onRewardCenterOpened", null);
+    }
+
+    @Override
+    public void onRewardCenterClosed() {
+        sendEvent(this.reactContext, "onRewardCenterClosed", null);
+    }
+
+    /* Lifecycle methods */
+
+    @Override
+    public void onHostResume() {
+        if (isAppInitialized) {
+            TheoremReach.getInstance().onResume(getCurrentActivity());
+        }
+    }
+
+    @Override
+    public void onHostPause() {
+        TheoremReach.getInstance().onPause();
+    }
+
+    @Override
+    public void onHostDestroy() {
+        // Actvity `onDestroy`
+    }
 }
