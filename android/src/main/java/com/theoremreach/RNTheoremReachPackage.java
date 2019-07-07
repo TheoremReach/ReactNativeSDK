eb1cfea58da65214635cd66764259e8b94b5da4f
Tom Hammond <tom@vinhub.io>
2019-07-07 12:32:48 -0500

    updating to 3.3.4

--- android/src/main/java/com/theoremreach/RNTheoremReachPackage.java
+++ android/src/main/java/com/theoremreach/RNTheoremReachPackage.java
@@ -1,6 +1,6 @@
 package com.theoremreach;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -10,18 +10,19 @@ import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.ViewManager;
 import com.facebook.react.bridge.JavaScriptModule;
 public class RNTheoremReachPackage implements ReactPackage {
-    @Override
-    public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
-      return Arrays.<NativeModule>asList(new RNTheoremReachModule(reactContext));
-    }
-
-    @Override
-    public List<Class<? extends JavaScriptModule>> createJSModules() {
-      return Collections.emptyList();
-    }
 
     @Override
     public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
       return Collections.emptyList();
+    }
+
+    @Override
+    public List<NativeModule> createNativeModules(
+                                ReactApplicationContext reactContext) {
+      List<NativeModule> modules = new ArrayList<>();
+
+      modules.add(new RNTheoremReachModule(reactContext));
+
+      return modules;
     }
 }
